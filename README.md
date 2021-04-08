49bfedc29d9274ab2546156c3b931fbae2911140
Polo Li <pololi@stripe.com>
2021-04-07 21:20:32 -0700

    pl - add Boleto demo

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Stripe Checkout is the fastest way to get started with payments. Included are some basic build and run scripts you can use to start up the application.
 
+## Demo link
+https://boleto-checkout.herokuapp.com/
+Note: for test mode, you can use `000.000.000-00` for CNPJ/CPF field
+
 ## Running the sample
 
 1. Build the server
@@ -10,10 +14,12 @@ Stripe Checkout is the fastest way to get started with payments. Included are so
 bundle install
 ```
 
-2. Run the server
+2. Make a copy `.env.template` to `.env` and update the file with your Stripe API keys
+
+3. Run the server
 
 ```
-ruby server.rb -o 0.0.0.0
+ruby server.rb
 ```
 
-3. Go to [http://localhost:4242/checkout.html](http://localhost:4242/checkout.html)
+4. Go to [http://localhost:4242](http://localhost:4242)
--- server.rb
+++ server.rb
@@ -1,35 +1,98 @@
+# frozen_string_literal: true
+
 require 'stripe'
 require 'sinatra'
 
-# This is a sample test API key. Sign in to see examples pre-filled with your key.
-Stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+unless ENV['APP_ENV'] == 'production'
+  require 'dotenv'
+  Dotenv.load('.env')
+end
+Stripe.api_key = ENV['STRIPE_TEST_SECRET_KEY']
+Stripe.api_version = '2020-08-27;boleto_beta=v1'
+ENVPOINT_SECRET = ENV['WEBHOOK_SIGNING_SECRET']
 
 set :static, true
 set :public_folder, File.dirname(__FILE__)
 set :port, 4242
 
-YOUR_DOMAIN = 'http://localhost:4242'
+LOCAL_DOMAIN = "http://localhost:#{settings.port}"
+YOUR_DOMAIN = ENV['APP_ENV'] == 'production' ? ENV['HEROKU_DOMAIN'] : LOCAL_DOMAIN
+
+get '/' do
+  erb :checkout, locals: { pubkey: ENV['STRIPE_TEST_PUBLIC_KEY'] }
+end
+
+post '/webhook' do
+  event = nil
+
+  logger.info('MY_LOG: Receive webhook request')
+
+  # Verify webhook signature and extract the event
+  # See https://stripe.com/docs/webhooks/signatures for more information.
+  begin
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    payload = request.body.read
+    event = Stripe::Webhook.construct_event(payload, sig_header, ENVPOINT_SECRET)
+  rescue JSON::ParserError => e
+    # Invalid payload
+    logger.error('MY_LOG: webhook Invalid payload')
+    return status 400
+  rescue Stripe::SignatureVerificationError => e
+    # Invalid signature
+    logger.error('MY_LOG: webhook Invalid signature')
+    return status 400
+  end
+
+  logger.info("MY_LOG: EVENT: #{event['type']} #{event.id}")
+
+  begin
+    case event['type']
+    when 'checkout.session.completed'
+      logger.info('MY_LOG: case EVENT checkout.session.completed')
+      checkout_session = event['data']['object']
+
+      # TODO: fill in with your own logic
+      logger.info("Fulfilling order for #{checkout_session.id}")
+    when 'payment_intent.requires_action'
+      logger.info('MY_LOG: case EVENT payment_intent.requires_action')
+      payment_intent = event['data']['object']
+      logger.info("MY_LOG: payment intent #{payment_intent}")
+    else
+      logger.error("MY_LOG: WTF event #{event['type']}")
+    end
+  rescue StandardError => e
+    logger.error("MY_LOG: WTF #{e.inspect}")
+    return status 200
+  end
+
+  return status 200
+end
 
 post '/create-checkout-session' do
   content_type 'application/json'
 
-  session = Stripe::Checkout::Session.create({
-    payment_method_types: ['card'],
+  session = Stripe::Checkout::Session.create(
+    payment_method_types: %w[card boleto],
+    payment_method_options: {
+      boleto: {
+        expires_after_days: 14
+      }
+    },
     line_items: [{
       price_data: {
         unit_amount: 2000,
-        currency: 'usd',
+        currency: 'brl',
         product_data: {
           name: 'Stubborn Attachments',
-          images: ['https://i.imgur.com/EHyR2nP.png'],
-        },
+          images: ['https://i.imgur.com/EHyR2nP.png']
+        }
       },
-      quantity: 1,
+      quantity: 1
     }],
     mode: 'payment',
     success_url: YOUR_DOMAIN + '/success.html',
-    cancel_url: YOUR_DOMAIN + '/cancel.html',
-  })
+    cancel_url: YOUR_DOMAIN + '/cancel.html'
+  )
 
   {
     id: session.id
